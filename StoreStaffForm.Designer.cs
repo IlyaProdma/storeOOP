0e6985f43f0172c9e7d25f64dd61f3d27f4826b6
IlyaProdma <idprodma@edu.hse.ru>
2021-12-06 11:37:48 +0300

    add staff user main form

--- StoreStaffForm.Designer.cs
+++ StoreStaffForm.Designer.cs
@@ -28,59 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.categoriesTree = new DarkUI.Controls.DarkTreeView();
             this.darkSectionPanel1 = new DarkUI.Controls.DarkSectionPanel();
             this.darkSectionPanel2 = new DarkUI.Controls.DarkSectionPanel();
-            this.darkTreeView1 = new DarkUI.Controls.DarkTreeView();
+            this.productsListView = new System.Windows.Forms.ListView();
+            this.columnImage = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnRetailPrice = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.darkButton1 = new DarkUI.Controls.DarkButton();
             this.darkSectionPanel1.SuspendLayout();
+            this.darkSectionPanel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // categoriesTree
+            // 
+            this.categoriesTree.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categoriesTree.Location = new System.Drawing.Point(1, 25);
+            this.categoriesTree.MaxDragChange = 20;
+            this.categoriesTree.Name = "categoriesTree";
+            this.categoriesTree.Size = new System.Drawing.Size(199, 334);
+            this.categoriesTree.TabIndex = 0;
+            this.categoriesTree.Text = "Категории";
+            this.categoriesTree.SelectedNodesChanged += new System.EventHandler(this.categoriesTree_SelectedNodesChanged);
+            this.categoriesTree.MouseClick += new System.Windows.Forms.MouseEventHandler(this.categoriesTree_MouseClick);
             // 
             // darkSectionPanel1
             // 
-            this.darkSectionPanel1.Controls.Add(this.darkTreeView1);
+            this.darkSectionPanel1.Controls.Add(this.darkButton1);
+            this.darkSectionPanel1.Controls.Add(this.categoriesTree);
             this.darkSectionPanel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.darkSectionPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.darkSectionPanel1.Location = new System.Drawing.Point(0, 0);
+            this.darkSectionPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.darkSectionPanel1.Location = new System.Drawing.Point(7, 6);
             this.darkSectionPanel1.Name = "darkSectionPanel1";
-            this.darkSectionPanel1.SectionHeader = "Tree of categories";
-            this.darkSectionPanel1.Size = new System.Drawing.Size(251, 450);
-            this.darkSectionPanel1.TabIndex = 0;
+            this.darkSectionPanel1.SectionHeader = "Категории товаров";
+            this.darkSectionPanel1.Size = new System.Drawing.Size(200, 450);
+            this.darkSectionPanel1.TabIndex = 1;
             // 
             // darkSectionPanel2
             // 
+            this.darkSectionPanel2.Controls.Add(this.productsListView);
             this.darkSectionPanel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.darkSectionPanel2.Location = new System.Drawing.Point(251, 0);
+            this.darkSectionPanel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.darkSectionPanel2.Location = new System.Drawing.Point(207, 6);
             this.darkSectionPanel2.Name = "darkSectionPanel2";
-            this.darkSectionPanel2.SectionHeader = null;
-            this.darkSectionPanel2.Size = new System.Drawing.Size(549, 305);
-            this.darkSectionPanel2.TabIndex = 1;
+            this.darkSectionPanel2.SectionHeader = "Выбранная категория:";
+            this.darkSectionPanel2.Size = new System.Drawing.Size(575, 450);
+            this.darkSectionPanel2.TabIndex = 2;
             // 
-            // darkTreeView1
+            // productsListView
             // 
-            this.darkTreeView1.Location = new System.Drawing.Point(0, 28);
-            this.darkTreeView1.MaxDragChange = 20;
-            this.darkTreeView1.Name = "darkTreeView1";
-            this.darkTreeView1.Size = new System.Drawing.Size(75, 23);
-            this.darkTreeView1.TabIndex = 0;
-            this.darkTreeView1.Text = "darkTreeView1";
+            this.productsListView.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.productsListView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.productsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnImage,
+            this.columnName,
+            this.columnRetailPrice});
+            this.productsListView.HideSelection = false;
+            this.productsListView.Location = new System.Drawing.Point(5, 37);
+            this.productsListView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.productsListView.MultiSelect = false;
+            this.productsListView.Name = "productsListView";
+            this.productsListView.Size = new System.Drawing.Size(570, 411);
+            this.productsListView.TabIndex = 0;
+            this.productsListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // columnImage
+            // 
+            this.columnImage.Text = "Изображение";
+            this.columnImage.Width = 157;
+            // 
+            // columnName
+            // 
+            this.columnName.Text = "Название";
+            this.columnName.Width = 436;
+            // 
+            // columnRetailPrice
+            // 
+            this.columnRetailPrice.Text = "Цена (розн.)";
+            this.columnRetailPrice.Width = 537;
+            // 
+            // darkButton1
+            // 
+            this.darkButton1.Location = new System.Drawing.Point(5, 366);
+            this.darkButton1.Name = "darkButton1";
+            this.darkButton1.Padding = new System.Windows.Forms.Padding(5);
+            this.darkButton1.Size = new System.Drawing.Size(189, 52);
+            this.darkButton1.TabIndex = 1;
+            this.darkButton1.Text = "Заказы";
             // 
             // StoreStaffForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(789, 462);
             this.Controls.Add(this.darkSectionPanel2);
             this.Controls.Add(this.darkSectionPanel1);
             this.Name = "StoreStaffForm";
+            this.Padding = new System.Windows.Forms.Padding(7, 6, 7, 6);
             this.Text = "StoreStaffForm";
             this.darkSectionPanel1.ResumeLayout(false);
+            this.darkSectionPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private DarkUI.Controls.DarkTreeView categoriesTree;
         private DarkUI.Controls.DarkSectionPanel darkSectionPanel1;
-        private DarkUI.Controls.DarkTreeView darkTreeView1;
         private DarkUI.Controls.DarkSectionPanel darkSectionPanel2;
+        private System.Windows.Forms.ListView productsListView;
+        private System.Windows.Forms.ColumnHeader columnImage;
+        private System.Windows.Forms.ColumnHeader columnName;
+        private System.Windows.Forms.ColumnHeader columnRetailPrice;
+        private DarkUI.Controls.DarkButton darkButton1;
     }
 }
--- StoreStaffForm.cs
+++ StoreStaffForm.cs
@@ -3,19 +3,148 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DarkUI.Controls;
 using DarkUI.Forms;
+using Newtonsoft.Json;
 
 namespace Store
 {
     public partial class StoreStaffForm : DarkForm
     {
+        private DarkTreeNode MakeNode(Category category)
+        {
+            DarkTreeNode node = new DarkTreeNode(category.Name);
+            if (category.Subcategories != null)
+                foreach (Subcategory subcategory in category.Subcategories)
+                    node.Nodes.Add(new DarkTreeNode(subcategory.Name));
+            return node;
+        }
+
+        private void SetTreeview(List<Category> categories)
+        {
+            categoriesTree.Nodes.Clear();
+            List<DarkTreeNode> treeNodes = new List<DarkTreeNode>();
+            for (int i = 0; i < categories.Count; i++)
+            {
+                treeNodes.Add(MakeNode(categories[i]));
+                categoriesTree.Nodes.Add(treeNodes[i]);
+            }
+        }
+
+        private List<Category> readCategories()
+            => JsonConvert.DeserializeObject<List<Category>>(File.ReadAllText("data/categories.json"));
+
+        private List<Product> readProductsByCategory(string category)
+            => JsonConvert.DeserializeObject<List<Product>>(File.ReadAllText("data/products.json"))
+                .Where<Product>(p => p.Category == category || p.Subcategory == category).ToList();
+
+        private string readImage(List<JsonImage> images, string vendorCode)
+            => images.Where<JsonImage>(im => im.VendorCode == vendorCode).ElementAt(0).Image;
+
+        private Product readProductByCode(string vendorCode)
+            => JsonConvert.DeserializeObject<List<Product>>(File.ReadAllText("data/products.json"))
+            .Where<Product>(p => p.VendorCode == vendorCode).ElementAt(0);
+
+        private Customer readCustomerByLogin(string Login)
+            => JsonConvert.DeserializeObject<List<Customer>>(File.ReadAllText("data/customers.json"))
+            .Where<Customer>(cs => cs.Login == Login).ElementAt(0);
         public StoreStaffForm()
         {
             InitializeComponent();
+            List<Category> categories = readCategories();
+            SetTreeview(categories);
+        }
+
+        private void categoriesTree_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                ContextMenu actions = new ContextMenu(new MenuItem[] { new MenuItem() { Text = "Новая категория"},
+                                                                       new MenuItem() { Text = "Новая подкатегория" }});
+                actions.MenuItems[0].Click += createNewCategory;
+                actions.MenuItems[1].Click += createNewSubcategory;
+                if (categoriesTree.SelectedNodes.Count > 0)
+                {
+                    actions.MenuItems.Add(new MenuItem() { Text = "Редактировать" });
+                    actions.MenuItems[2].Click += redactCategory;
+                }
+                actions.Show(this, new Point(e.X, e.Y));
+            }
+        }
+
+        private void createNewCategory(object sender, EventArgs e)
+        {
+            NewCategoryForm newCategory = new NewCategoryForm(true);
+            newCategory.ShowDialog();
+            List<Category> categories = readCategories();
+            SetTreeview(categories);
+            productsListView.Items.Clear();
+        }
+
+        private void createNewSubcategory(object sender, EventArgs e)
+        {
+            NewCategoryForm newSubcategory = new NewCategoryForm(false);
+            newSubcategory.ShowDialog();
+            List<Category> categories = readCategories();
+            SetTreeview(categories);
+            productsListView.Items.Clear();
+        }
+
+        private void redactCategory(object sender, EventArgs e)
+        {
+            List<Category> categories = JsonConvert.DeserializeObject<List<Category>>(File.ReadAllText("data/categories.json"));
+            if (categories.Find(cat => cat.Name.Equals(categoriesTree.SelectedNodes[0].Text)) != null)
+            {
+                RedactCategoryForm redactCategory = new RedactCategoryForm(categoriesTree.SelectedNodes[0].Text, true);
+                redactCategory.ShowDialog();
+            }
+            else
+            {
+                RedactCategoryForm redactCategory = new RedactCategoryForm(categoriesTree.SelectedNodes[0].Text,
+                                                                           false,
+                                                                           categoriesTree.SelectedNodes[0].ParentNode.Text);
+                redactCategory.ShowDialog();
+            }
+            categories = readCategories();
+            SetTreeview(categories);
+            productsListView.Items.Clear();
+        }
+
+        private void categoriesTree_SelectedNodesChanged(object sender, EventArgs e)
+        {
+            if (categoriesTree.SelectedNodes.Count > 0)
+            {
+                List<Product> products = readProductsByCategory(categoriesTree.SelectedNodes[0].Text);
+                darkSectionPanel2.SectionHeader = "Выбранная категория: " + categoriesTree.SelectedNodes[0].Text;
+                productsListView.Items.Clear();
+                ImageList imageList = new ImageList();
+                imageList.ImageSize = new Size(200, 200);
+                List<JsonImage> images = JsonConvert.DeserializeObject<List<JsonImage>>(File.ReadAllText("data/images.json"));
+                foreach (Product product in products)
+                {
+                    imageList.Images.Add(new Bitmap(readImage(images, product.VendorCode)));
+                }
+
+                Bitmap emptyImage = new Bitmap(200, 200);
+                using (Graphics gr = Graphics.FromImage(emptyImage))
+                {
+                    gr.Clear(Color.White);
+                }
+                imageList.Images.Add(emptyImage);
+                productsListView.LargeImageList = imageList;
+                for (int i = 0; i < products.Count; ++i)
+                {
+                    ListViewItem item = new ListViewItem(new string[] { String.Format("{0} - {1}р", products[i].Name, products[i].PriceRetail.ToString()),
+                                                                        products[i].VendorCode, readImage(images, products[i].VendorCode)});
+                    item.ImageIndex = i;
+                    productsListView.Items.Add(item);
+                }
+            }
         }
     }
 }
